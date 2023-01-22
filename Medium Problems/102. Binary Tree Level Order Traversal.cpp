2b9ceb40bc161a83122f253797742f83f406c89d
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-01-23 00:49:51 +0530

    Updating

--- Medium Problems/102. Binary Tree Level Order Traversal.cpp
+++ Medium Problems/102. Binary Tree Level Order Traversal.cpp
@@ -5,11 +5,14 @@
 using namespace std;
 
 
-//   Definition for singly-linked list.
-  struct ListNode {
+//   Definition for a binary tree node.
+  struct TreeNode {
       int val;
-      ListNode *next;
-      ListNode(int x) : val(x), next(NULL) {}
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
   };
 
 class Solution {
