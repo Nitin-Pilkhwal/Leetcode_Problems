7795f869ac5ce4cc6fc2abab0f906881b5e8dcbf
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-01-22 09:56:42 +0530

    Create 944. Delete Columns to Make Sorted.cpp

---
+++ Easy Problems/944. Delete Columns to Make Sorted.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int count=0;
+        int row=strs.size(),col=strs[0].size();
+        for(int j=0;j<col;j++){
+            for(int i=0;i<row-1;i++){
+                if(strs[i][j]>strs[i+1][j]){
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+};
