507a1be98338a24b26081ac93f6e81adf435a6f4
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-01-25 17:05:23 +0530

    Create 226. Invert Binary Tree.cpp

---
+++ Easy Problems/226. Invert Binary Tree.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+//   Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return root;
+        TreeNode* temp;
+        temp=root->right;
+        root->right=root->left;
+        root->left=temp;
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
