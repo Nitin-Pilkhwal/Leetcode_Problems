3fd6208706e938e143e02d92485657943c42c0df
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-01-27 22:52:07 +0530

    Create 997. Find the Town Judge .cpp

---
+++ Easy Problems/997. Find the Town Judge.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> a(n,0);
+        vector<int> b(n,0);
+        for(auto& t: trust){
+            a[t[0]-1]++;
+            b[t[1]-1]++;
+        }
+        for(int i=0;i<n;i++){
+            if(a[i]==0 && b[i]==n-1) return i+1;
+        }
+        return -1;
+    }
+};
