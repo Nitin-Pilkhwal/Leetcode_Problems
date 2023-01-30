f39ede6202e4fd6bbf9b5b29a89f2002cd8fef78
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-01-30 11:03:16 +0530

    Create 2549. Count Distinct Numbers on Board .cpp

---
+++ Easy Problems/2549. Count Distinct Numbers on Board.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int distinctIntegers(int n) {
+        return n==1 ? 1:n-1;
+    }
+};
