1c92faf3c67e9b1fb90421aeaef6520aa461c2de
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-02-01 23:32:44 +0530

    Create 2396. Strictly Palindromic Number.cpp

---
+++ Medium Problems/2396. Strictly Palindromic Number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+    }
+};
