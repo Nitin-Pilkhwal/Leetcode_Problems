52416b8f806ac10d3eb803108f1f9bc48a72fb98
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-03-01 19:05:07 +0530

    Create 268. Missing Number.cpp

---
+++ Easy Problems/268. Missing Number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+         sort(nums.begin(),nums.end());
+         int i;
+         for(i=0;i<nums.size();i++){
+             if(i!=nums[i]) return i;
+         }
+         return i;
+    }
+};
