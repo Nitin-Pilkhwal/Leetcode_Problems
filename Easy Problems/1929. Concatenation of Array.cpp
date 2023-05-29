3d93ff6400b75e53c8c86e9997a8874c071ff4c2
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-05-29 21:39:41 +0530

    Create 1929. Concatenation of Array.cpp

---
+++ Easy Problems/1929. Concatenation of Array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(2*n);
+        for(int i=0;i<n;i++){
+            ans[i]=nums[i];
+            ans[i+n]=nums[i];
+        }
+        return ans;
+    }
+};
