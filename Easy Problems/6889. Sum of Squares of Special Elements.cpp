f735264ec57444ccb0e1b6b2e24224118321c98d
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-07-24 11:12:41 +0530

    Create 6889. Sum of Squares of Special Elements.cpp

---
+++ Easy Problems/6889. Sum of Squares of Special Elements.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int sumOfSquares(vector<int>& nums) {
+        int ans=0;
+        int n=nums.size();
+        for(int i=0;i<nums.size();i++){
+            if(n%(i+1)==0){
+                ans+=(nums[i]*nums[i]);
+            }
+        }
+        return ans;
+    }
+};
