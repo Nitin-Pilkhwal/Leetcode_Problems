6b7d7e1d3a1700157086b69fe0a7bcd70a6b0eb3
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-08-27 21:13:42 +0530

    Create 61. Rotate List.cpp

---
+++ Medium Problems/61. Rotate List.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || k==0) return head;
+        ListNode* curr = head;
+        int len = 1;
+        while(curr->next){
+            len++;
+            curr=curr->next;
+        }
+        curr->next = head;
+        k=k%len;
+        k=len-k;
+        while(k--) curr=curr->next;
+        head=curr->next;
+        curr->next = NULL;
+        return head;
+    }
+};
